e86d4bff6895182e556dd4c49b361c93a0e623c7
Marcelo Sarinho <marcelo.sarinho@fatec.sp.gov.br>
2023-07-09 22:53:55 -0300

    criacao do icone de adicionar

--- src/pages/Pedidos.js
+++ src/pages/Pedidos.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Header } from "../components/Header";
 import { Navbar } from "../components/Navbar";
-import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { faFilter, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export function Pedidos() {
   return (
@@ -15,15 +15,18 @@ export function Pedidos() {
             name="filtro"
             className="text-sm font-bold text-menu-gray py-2 px-1 rounded-full"
           >
-            <option value="" disabled>
+            <option value="" selected disabled>
               Selecione os filtros aqui
             </option>
             <option value="filtro1">Filtro 1</option>
             <option value="filtro2">Filtro 2</option>
           </select>
         </div>
-        <div>
-          Oi
+        <div className="relative top-9 left-32">
+          <FontAwesomeIcon
+            icon={faPlus}
+            className="bg-success p-3 rounded-full w-7 h-7"
+          />
         </div>
       </div>
       <Navbar />
