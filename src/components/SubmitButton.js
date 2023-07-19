facc5209277a41944c1b47e9050ab9794a8cc960
Marcelo Sarinho <marcelo.sarinho@fatec.sp.gov.br>
2023-07-18 21:44:01 -0300

    criacao da pagina de nova doacao

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,10 +1,10 @@
 export function SubmitButton(props) {
-  const { type, children } = props;
+  const { type, children, className } = props;
 
   return (
     <button
       type={type}
-      className="mt-2 bg-primary px-5 py-2 rounded-lg font-bold text-white text-xl"
+      className={`mt-2 bg-primary px-5 py-2 rounded-lg font-bold text-white text-xl ${className}`}
     >
       {children}
     </button>
--- src/pages/NovaDoacao.js
+++ src/pages/NovaDoacao.js
@@ -78,7 +78,7 @@ export function NovaDoacao() {
 
         <h1 className="text-menu-gray font-medium mb-1">Disponibilidade</h1>
         <p className="text-xs text-menu-gray text-opacity-80 mb-2">
-          Adicione o período em dias e horasque você terá disponibilidade para
+          Adicione o período em dias e horas que você terá disponibilidade para
           entregar os produtos do seu anúncio
         </p>
 
@@ -116,9 +116,50 @@ export function NovaDoacao() {
           />
         </div>
 
-        <hr className=""/>
+        <hr className="my-4" />
 
-        <SubmitButton>Continuar</SubmitButton>
+        <h1 className="text-menu-gray font-medium mb-1">Local de entrega</h1>
+        <p className="text-xs text-menu-gray text-opacity-80 mb-2">
+          Adicione aqui o local que você gostaria de se encontrar com os
+          interessados para a transação dos itens do anúncio
+        </p>
+
+        <FormLabel name="cep">CEP</FormLabel>
+        <FormInput placeholder="00000-00" name="cep" type="text" id="cep" />
+
+        <FormLabel name="endereco">Endereço</FormLabel>
+        <FormInput name="endereco" type="text" id="endereco" />
+
+        <div className="flex gap-4">
+          <div className="w-2/4">
+            <FormLabel name="numero">Número</FormLabel>
+            <FormInput
+              className="w-full"
+              name="numero"
+              type="text"
+              id="numero"
+            />
+          </div>
+
+          <div className="w-2/4">
+            <FormLabel name="complemento">Complemento</FormLabel>
+            <FormInput
+              className="w-full"
+              name="complemento"
+              type="text"
+              id="complemento"
+            />
+          </div>
+        </div>
+
+        <FormLabel name="pontoReferencia">Ponto de Referência</FormLabel>
+        <FormInput
+          className="w-full"
+          name="pontoReferencia"
+          type="text"
+          id="pontoReferencia"
+        />
+        <SubmitButton className="w-fit px-20 mx-auto my-20">Finalizar</SubmitButton>
       </form>
       <Navbar />
     </>
